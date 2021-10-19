2c03623aac95e52623b7288e7898ac20e653e066
Matej Sychra <suculent@me.com>
2021-10-19 14:03:04 +0200

    Initial Commit

---
+++ Shared/houskaApp.swift
@@ -0,0 +1,17 @@
+//
+//  houskaApp.swift
+//  Shared
+//
+//  Created by Matěj Sychra on 19.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct houskaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
